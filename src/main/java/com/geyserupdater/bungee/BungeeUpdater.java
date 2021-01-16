af89cb692248f669760e5669a9b5a6b4ecb3c66e
Jens <jenscollaertprive@hotmail.com>
2021-01-16 08:53:24 +0100

    *Added MoveGeyser Methode using filestreams to copy new build over old build *Added DeleteBuild because new build is copied and not moved.

--- src/main/java/com/geyserupdater/bungee/BungeeUpdater.java
+++ src/main/java/com/geyserupdater/bungee/BungeeUpdater.java
@@ -8,6 +8,7 @@ import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
+import org.geysermc.connector.utils.FileUtils;
 import org.geysermc.connector.utils.LanguageUtils;
 
 
@@ -37,30 +38,15 @@ public final class BungeeUpdater extends Plugin {
         getProxy().getPluginManager().getPlugin("Geyser-BungeeCord").onDisable();
         try {
             this.MoveGeyser();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+        try {
+            this.DeleteBuild();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public void MoveGeyser() throws IOException {
-            File fileToCopy = new File("plugins/GeyserUpdater/BuildUpdate/Geyser-BungeeCord.jar");
-            FileInputStream input = new FileInputStream(fileToCopy);
-
-            File newFile = new File("plugins/Geyser-BungeeCord.jar");
-            FileOutputStream output = new FileOutputStream(newFile);
-
-            byte[] buf = new byte[1024];
-            int bytesRead;
-
-            while ((bytesRead = input.read(buf)) > 0) {
-                output.write(buf, 0, bytesRead);
-            }
-
-            input.close();
-            output.close();
-        }
 
     public void onConfig() {
         try {
@@ -88,6 +74,30 @@ public final class BungeeUpdater extends Plugin {
             getProxy().getScheduler().schedule(this, () -> AutoUpdateGeyser.checkUpdate(getProxy().getConsole()), 0, 24, TimeUnit.HOURS);
         }
     }
+
+    public void MoveGeyser() throws IOException {
+        File fileToCopy = new File("plugins/GeyserUpdater/BuildUpdate/Geyser-BungeeCord.jar");
+        FileInputStream input = new FileInputStream(fileToCopy);
+
+        File newFile = new File("plugins/Geyser-BungeeCord.jar");
+        FileOutputStream output = new FileOutputStream(newFile);
+
+        byte[] buf = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = input.read(buf)) > 0) {
+            output.write(buf, 0, bytesRead);
+        }
+
+        input.close();
+        output.close();
+    }
+
+    private void DeleteBuild() throws IOException {
+        File file = new File("plugins/GeyserUpdater/BuildUpdate/Geyser-BungeeCord.jar");
+        Files.delete(file.toPath());
+    }
+
 
     public Configuration getConfiguration() {
         return configuration;
