8598afafc6dc6a21277ac029343d52ed59c3dd9b
Jens <jenscollaertprive@hotmail.com>
2021-02-09 17:34:43 +0100

    Updating code for GeyserUpdater itself. Working

--- src/main/java/com/alysaa/geyserupdater/common/util/ResourceUpdaterSpigot.java
+++ src/main/java/com/alysaa/geyserupdater/common/util/ResourceUpdaterSpigot.java
@@ -21,7 +21,7 @@ public class ResourceUpdaterSpigot {
 
     public void getVersion(final Consumer<String> consumer) {
         Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=88555" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
                 if (scanner.hasNext()) {
                     consumer.accept(scanner.next());
                 }
