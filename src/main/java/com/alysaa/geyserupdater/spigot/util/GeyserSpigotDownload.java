8033561e1e3c9ad675619c9b1e7937c40d87cc60
Jens <jenscollaertprive@hotmail.com>
2021-02-25 20:22:27 +0100

    Changed thread to whole download class spigot/paper

--- src/main/java/com/alysaa/geyserupdater/spigot/util/GeyserSpigotDownload.java
+++ src/main/java/com/alysaa/geyserupdater/spigot/util/GeyserSpigotDownload.java
@@ -12,61 +12,61 @@ import java.net.URLConnection;
 
 public class GeyserSpigotDownload {
     public static void GeyserDownload() {
-        OutputStream os = null;
-        InputStream is = null;
-        String fileUrl = "https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar";
-        String outputPath = ("plugins/update/Geyser-Spigot.jar");
-        try {
-            // create a url object
-            URL url = new URL(fileUrl);
-            // connection to the file
-            URLConnection connection = url.openConnection();
-            // get input stream to the file
-            is = connection.getInputStream();
-            // get output stream to download file
-            os = new FileOutputStream(outputPath);
-            final byte[] b = new byte[2048];
-            int length;
-            // read from input stream and write to output stream
-            while ((length = is.read(b)) != -1) {
-                os.write(b, 0, length);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            // close streams
-            if (os != null) {
+        Runnable runnable = () -> {
+            try {
+                OutputStream os = null;
+                InputStream is = null;
+                String fileUrl = "https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar";
+                String outputPath = ("plugins/update/Geyser-Spigot.jar");
                 try {
-                    os.close();
+                    // create a url object
+                    URL url = new URL(fileUrl);
+                    // connection to the file
+                    URLConnection connection = url.openConnection();
+                    // get input stream to the file
+                    is = connection.getInputStream();
+                    // get output stream to download file
+                    os = new FileOutputStream(outputPath);
+                    final byte[] b = new byte[2048];
+                    int length;
+                    // read from input stream and write to output stream
+                    while ((length = is.read(b)) != -1) {
+                        os.write(b, 0, length);
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
+                } finally {
+                    // close streams
+                    if (os != null) {
+                        try {
+                            os.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        CheckBuildFile.checkSpigotFile();
+                    }
                 }
-            }
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                CheckBuildFile.checkSpigotFile();
-            }
-        }
-        if (SpigotUpdater.plugin.getConfig().getBoolean("Auto-Restart-Server")) {
+                if (SpigotUpdater.plugin.getConfig().getBoolean("Auto-Restart-Server")) {
 
-            SpigotUpdater.plugin.getLogger().info("[GeyserUpdater] The Server will restart in 10 seconds!");
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',SpigotUpdater.getPlugin().getConfig().getString("Restart-Message-Players")));
-            }
-            Runnable runnable = () -> {
-                try {
+                    SpigotUpdater.plugin.getLogger().info("[GeyserUpdater] The Server will restart in 10 seconds!");
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', SpigotUpdater.getPlugin().getConfig().getString("Restart-Message-Players")));
+                    }
                     Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
-            };
-            Thread thread = new Thread(runnable);
-            thread.start();
-            Bukkit.spigot().restart();
-        }
+                Bukkit.spigot().restart();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }
