61ef7463d03f37c1733aa18f58c7e671b10c088e
Konicai <71294714+Konicai@users.noreply.github.com>
2021-02-25 22:20:08 -0500

    allow console to run geyserupdate command

--- src/main/java/com/alysaa/geyserupdater/spigot/command/GeyserCommand.java
+++ src/main/java/com/alysaa/geyserupdater/spigot/command/GeyserCommand.java
@@ -1,13 +1,15 @@
 package com.alysaa.geyserupdater.spigot.command;
 
 import com.alysaa.geyserupdater.common.util.CheckBuildNum;
+import com.alysaa.geyserupdater.spigot.SpigotUpdater;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
-import java.io.*;
+import java.io.IOException;
 
 public class GeyserCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -20,6 +22,13 @@ public class GeyserCommand implements CommandExecutor {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+        } else if (sender instanceof ConsoleCommandSender) {
+            SpigotUpdater.plugin.getLogger().info("Checking current Geyser version!");
+            try {
+                CheckBuildNum.checkBuildNumberSpigot();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
         return false;
