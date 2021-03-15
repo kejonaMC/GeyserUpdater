72d5c87fe90006cd9d216c3b7bc5ce2a42b43732
Jens <jenscollaertprive@hotmail.com>
2021-03-15 09:06:33 +0100

    Added Velocity support

---
+++ src/main/java/com/alysaa/geyserupdater/velocity/util/OSUtils.java
@@ -0,0 +1,35 @@
+package com.alysaa.geyserupdater.velocity.util;
+
+public class OSUtils {
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    public static boolean isMac() {
+        return (OS.indexOf("mac") >= 0);
+    }
+
+    public static boolean isLinux() {
+        return (OS.indexOf("nix") >= 0
+                || OS.indexOf("nux") >= 0
+                || OS.indexOf("aix") > 0);
+    }
+
+    public static OSType getOS() {
+        if (isLinux()) return OSType.LINUX;
+        else if (isMac()) return OSType.MACOS;
+        else if (isWindows()) return OSType.WINDOWS;
+        else return null;
+    }
+}
+
+enum OSType {
+    WINDOWS("windows"),
+    MACOS("macos"),
+    LINUX("linux");
+
+    OSType(String windows) {}
+}
