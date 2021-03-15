bde4fc357a1feaac33ee427df4a52f51ee91598b
Jens <jenscollaertprive@hotmail.com>
2021-03-15 23:16:36 +0100

    Cleanup velocity code. removed classes that where common.

--- src/main/java/com/alysaa/geyserupdater/velocity/command/GeyserUpdaterCommand.java
+++ src/main/java/com/alysaa/geyserupdater/velocity/command/GeyserUpdaterCommand.java
@@ -1,23 +1,19 @@
 package com.alysaa.geyserupdater.velocity.command;
 
-import com.alysaa.geyserupdater.velocity.util.BuildNumChecker;
+import com.alysaa.geyserupdater.common.util.CheckBuildNum;
 import com.velocitypowered.api.command.SimpleCommand;
 import net.kyori.adventure.text.Component;
 
-import java.io.IOException;
-
 public class GeyserUpdaterCommand implements SimpleCommand {
-
     @Override
     public void execute(final Invocation invocation) {
         invocation.source().sendMessage(Component.text("[GeyserUpdater] Checking current Geyser version!"));
         try {
-            BuildNumChecker.checkBuildNumberVelocity();
+            CheckBuildNum.checkBuildNumberVelocity();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
     @Override
     public boolean hasPermission(final Invocation invocation) {
         return invocation.source().hasPermission("gupdater.geyserupdate");
--- src/main/java/com/alysaa/geyserupdater/velocity/util/VelocityJoinListener.java
+++ src/main/java/com/alysaa/geyserupdater/velocity/util/VelocityJoinListener.java
@@ -1,5 +1,6 @@
 package com.alysaa.geyserupdater.velocity.util;
 
+import com.alysaa.geyserupdater.common.util.CheckBuildFile;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
 import net.kyori.adventure.text.Component;
@@ -9,7 +10,7 @@ public class VelocityJoinListener {
     @Subscribe
     public void onPostLogin(PostLoginEvent event) {
         // We allow a cached result of maximum age 30 minutes to be used
-        if (BuildFileChecker.checkVelocityFile(true)) {
+        if (CheckBuildFile.checkVelocityFile(true)) {
             if (event.getPlayer().hasPermission("gupdater.geyserupdate")) {
                 event.getPlayer().sendMessage(Component.text("[GeyserUpdater] New Geyser build has been downloaded! Velocity restart is required!"));
             }
