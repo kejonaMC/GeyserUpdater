eb662535dfefefbbd0616c91490c768baa543d38
Jens <jenscollaertprive@hotmail.com>
2021-03-19 08:40:41 +0100

    Version Update V1.3.0

--- src/main/java/com/alysaa/geyserupdater/common/util/CheckBuildFile.java
+++ src/main/java/com/alysaa/geyserupdater/common/util/CheckBuildFile.java
@@ -9,14 +9,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class CheckBuildFile {
-
-
     // Epoch time at which the last direct build file check occurred.
     // Set to 0 first so a direct check always occurs first.
     private static long callTime = 0;
 
     private static boolean cachedResult;
 
+    public static boolean checkVelocityFile(boolean forPlayer) {
+        if (forPlayer) {
+            long elapsedTime = System.currentTimeMillis() - callTime;
+            if (elapsedTime < 30 * 60 * 1000) {
+                return cachedResult;
+            }
+        }
+        callTime = System.currentTimeMillis();
+        Path p = Paths.get("plugins/GeyserUpdater/BuildUpdate/Geyser-Velocity.jar");
+        boolean exists = Files.exists(p);
+        if (exists) {
+            VelocityUpdater.logger.warn("New Geyser build has been downloaded! Velocity restart is required!");
+            cachedResult = true;
+            return true;
+        }
+        cachedResult = false;
+        return false;
+    }
     public static boolean checkBungeeFile(boolean forPlayer) {
         if (forPlayer) {
             long elapsedTime = System.currentTimeMillis() - callTime;
@@ -47,24 +63,6 @@ public class CheckBuildFile {
         boolean exists = Files.exists(p);
         if (exists) {
             SpigotUpdater.plugin.getLogger().info("New Geyser build has been downloaded! Server restart is required!");
-            cachedResult = true;
-            return true;
-        }
-        cachedResult = false;
-        return false;
-    }
-    public static boolean checkVelocityFile(boolean forPlayer) {
-        if (forPlayer) {
-            long elapsedTime = System.currentTimeMillis() - callTime;
-            if (elapsedTime < 30 * 60 * 1000) {
-                return cachedResult;
-            }
-        }
-        callTime = System.currentTimeMillis();
-        Path p = Paths.get("plugins/GeyserUpdater/BuildUpdate/Geyser-Velocity.jar");
-        boolean exists = Files.exists(p);
-        if (exists) {
-            VelocityUpdater.logger.warn("New Geyser build has been downloaded! Velocity restart is required!");
             cachedResult = true;
             return true;
         }
