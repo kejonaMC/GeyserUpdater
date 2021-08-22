eadbb4c5e110620c91eed301a69fcc7789e2686f
Konicai <71294714+konicai@users.noreply.github.com>
2021-08-21 22:19:52 -0400

    fix velocity

--- src/main/java/com/projectg/geyserupdater/velocity/VelocityUpdater.java
+++ src/main/java/com/projectg/geyserupdater/velocity/VelocityUpdater.java
@@ -27,35 +27,39 @@ public class VelocityUpdater implements UpdaterBootstrap {
 
     public static final String VERSION = "1.6.0";
 
-    private final GeyserUpdater updater;
+    private GeyserUpdater updater;
     private final ProxyServer server;
+    private final Path dataDirectory;
+    private final Logger logger;
     private final Metrics.Factory metricsFactory;
 
     @Inject
-    public VelocityUpdater(ProxyServer server, Logger baseLogger, @DataDirectory final Path dataDirectory, Metrics.Factory metricsFactory) throws IOException {
+    public VelocityUpdater(ProxyServer server, Logger baseLogger, @DataDirectory final Path dataDirectory, Metrics.Factory metricsFactory) {
         this.server  = server;
+        this.dataDirectory = dataDirectory;
+        this.logger = baseLogger;
         this.metricsFactory = metricsFactory;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) throws IOException {
 
         updater = new GeyserUpdater(
                 dataDirectory,
                 dataDirectory.resolve("BuildUpdate"),
                 dataDirectory.getParent(),
                 this,
-                new Slf4jUpdaterLogger(baseLogger),
+                new Slf4jUpdaterLogger(logger),
                 new VelocityScheduler(this),
                 new VelocityPlayerHandler(server),
                 VERSION,
                 "/artifact/bootstrap/velocity/target/Geyser-Velocity.jar",
                 "/artifact/bootstrap/velocity/target/floodgate-velocity.jar"
         );
-    }
-
-    @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-        metricsFactory.make(this, 10673);
 
         // Register our only command
         server.getCommandManager().register("geyserupdate", new GeyserUpdateCommand());
+        metricsFactory.make(this, 10673);
     }
 
     @Subscribe(order = PostOrder.LAST)
